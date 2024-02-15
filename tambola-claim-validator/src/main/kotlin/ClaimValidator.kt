34b31068e157f1aa4e0b287c257f15d01991c339
SuryatejaChakkilala <chakkilalasuryatejab@sahaj.ai>
2024-02-15 10:54:08 +0530

    added bottom row validator and tests

--- tambola-claim-validator/src/main/kotlin/ClaimValidator.kt
+++ tambola-claim-validator/src/main/kotlin/ClaimValidator.kt
@@ -16,9 +16,26 @@ class ClaimValidator(private val ticket: List<List<Int>>) {
         return false
     }
 
+    private fun bottomRowValidator(numbersAnnounced: List<Int>): Boolean {
+        val bottomRowElements = ticket[2].toSet()
+        var count = 0
+
+        numbersAnnounced.forEachIndexed { index, value ->
+            if (value in bottomRowElements) {
+                count++
+            }
+            if (count == bottomRowElements.size) {
+                return index == (numbersAnnounced.size - 1)
+            }
+        }
+
+        return false
+    }
+
     fun validate(numbersAnnounced: List<Int>, claim: String): Boolean {
         return when (claim) {
             "TOP_ROW" -> topRowValidator(numbersAnnounced)
+            "BOTTOM_ROW" -> bottomRowValidator(numbersAnnounced)
             else -> false
         }
     }
--- tambola-claim-validator/src/test/kotlin/TambolaTest.kt
+++ tambola-claim-validator/src/test/kotlin/TambolaTest.kt
@@ -34,4 +34,35 @@ class TambolaTest {
         assertFalse(result)
     }
 
+
+    @Test
+    fun `player made claim of bottom row immediately after crossing all the elements of bottom row should be accepted`() {
+        val ticket = listOf(
+            listOf(4, 16, 48, 63, 76),
+            listOf(7, 23, 38, 52, 80),
+            listOf(9, 25, 56, 64, 83)
+        )
+
+        val numbersAnnounced = listOf(90, 9, 44, 45, 25, 64, 22, 56, 88, 83)
+        val claimMade = "BOTTOM_ROW"
+
+        val result = ClaimValidator(ticket).validate(numbersAnnounced, claimMade)
+        assertTrue(result)
+    }
+
+    @Test
+    fun `player made claim of bottom row after missing to claim immediately after crossing the elements of bottom row should be rejected`() {
+        val ticket = listOf(
+            listOf(4, 16, 48, 63, 76),
+            listOf(7, 23, 38, 52, 80),
+            listOf(9, 25, 56, 64, 83)
+        )
+
+        val numbersAnnounced = listOf(90, 9, 44, 45, 25, 64, 22, 56, 88, 83, 11, 12, 13)
+        val claimMade = "BOTTOM_ROW"
+
+        val result = ClaimValidator(ticket).validate(numbersAnnounced, claimMade)
+        assertFalse(result)
+    }
+
 }
