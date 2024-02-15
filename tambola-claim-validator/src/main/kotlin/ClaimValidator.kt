a94214b05c8dc106547f283bc9239bfcb969d433
SuryatejaChakkilala <chakkilalasuryatejab@sahaj.ai>
2024-02-15 10:35:42 +0530

    added top row validator to ClaimValidator

--- tambola-claim-validator/src/main/kotlin/ClaimValidator.kt
+++ tambola-claim-validator/src/main/kotlin/ClaimValidator.kt
@@ -1,5 +1,12 @@
 class ClaimValidator(private val ticket: List<List<Int>>) {
 
+    fun topRowValidator(numbersAnnounced: List<Int>): Boolean {
+        val topRowElements = ticket[0].toSet()
+        val count = 0
+
+        return false
+    }
+
     fun validate(numbersAnnounced: List<Int>, claim: String): Boolean {
         return true
     }
