b27aa3361c440c72507a2de74aea27eece277327
SuryatejaChakkilala <chakkilalasuryatejab@sahaj.ai>
2024-02-15 11:04:59 +0530

    roll back to previous code before refactor

--- tambola-claim-validator/src/main/kotlin/ClaimValidator.kt
+++ tambola-claim-validator/src/main/kotlin/ClaimValidator.kt
@@ -1,14 +1,7 @@
-import Constants.ClaimConstants.BOTTOM_ROW
-import Constants.ClaimConstants.FIRST_FIVE
-import Constants.ClaimConstants.TOP_ROW
-import Constants.NumericConstants.FIVE
-import Constants.NumericConstants.ONE
-import Constants.NumericConstants.ZERO
-
 class ClaimValidator(ticket: List<List<Int>>) {
 
-    private val topRowElements = ticket[ZERO].toSet()
-    private val bottomRowElements = ticket[ZERO].toSet()
+    private val topRowElements = ticket[0].toSet()
+    private val bottomRowElements = ticket[2].toSet()
 
     private val allElements = mutableSetOf<Int>()
 
@@ -22,14 +15,14 @@ class ClaimValidator(ticket: List<List<Int>>) {
 
 
     private fun topRowValidator(numbersAnnounced: List<Int>): Boolean {
-        var count = ZERO
+        var count = 0
 
         numbersAnnounced.forEachIndexed { index, value ->
             if (value in topRowElements) {
                 count++
             }
             if (count == topRowElements.size) {
-                return index == (numbersAnnounced.size - ONE)
+                return index == (numbersAnnounced.size - 1)
             }
         }
 
@@ -37,14 +30,14 @@ class ClaimValidator(ticket: List<List<Int>>) {
     }
 
     private fun bottomRowValidator(numbersAnnounced: List<Int>): Boolean {
-        var count = ZERO
+        var count = 0
 
         numbersAnnounced.forEachIndexed { index, value ->
             if (value in bottomRowElements) {
                 count++
             }
             if (count == bottomRowElements.size) {
-                return index == (numbersAnnounced.size - ONE)
+                return index == (numbersAnnounced.size - 1)
             }
         }
 
@@ -52,14 +45,14 @@ class ClaimValidator(ticket: List<List<Int>>) {
     }
 
     private fun firstFiveValidator(numbersAnnounced: List<Int>): Boolean {
-        var count = ZERO
+        var count = 0
 
         numbersAnnounced.forEachIndexed { index, value ->
             if (value in allElements) {
                 count++
             }
-            if (count == FIVE) {
-                return index == (numbersAnnounced.size - ONE)
+            if (count == 5) {
+                return index == (numbersAnnounced.size - 1)
             }
         }
 
@@ -68,9 +61,9 @@ class ClaimValidator(ticket: List<List<Int>>) {
 
     fun validate(numbersAnnounced: List<Int>, claim: String): Boolean {
         return when (claim) {
-            TOP_ROW -> topRowValidator(numbersAnnounced)
-            BOTTOM_ROW -> bottomRowValidator(numbersAnnounced)
-            FIRST_FIVE -> firstFiveValidator(numbersAnnounced)
+            "TOP_ROW" -> topRowValidator(numbersAnnounced)
+            "BOTTOM_ROW" -> bottomRowValidator(numbersAnnounced)
+            "FIRST_FIVE" -> firstFiveValidator(numbersAnnounced)
             else -> false
         }
     }
