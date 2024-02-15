f024a2b3d22529a9c40e3bd586e3428b194ff386
Sathiyaraman-M <sathiyaraman2003@gmail.com>
2024-02-15 17:00:01 +0530

    Complete implementation for Middle-Row Claim Validation

--- tambola-claim-validator/src/main/kotlin/ClaimValidator.kt
+++ tambola-claim-validator/src/main/kotlin/ClaimValidator.kt
@@ -1,15 +1,18 @@
 import Constants.ClaimConstants.BOTTOM_ROW
 import Constants.ClaimConstants.FIRST_FIVE
+import Constants.ClaimConstants.MIDDLE_ROW
 import Constants.ClaimConstants.TOP_ROW
 import Constants.NumericConstants.FIVE
 import Constants.NumericConstants.ONE
 import Constants.NumericConstants.ZERO
 import Constants.TicketConstants.BOTTOM_ROW_INDEX
+import Constants.TicketConstants.MIDDLE_ROW_INDEX
 import Constants.TicketConstants.TOP_ROW_INDEX
 
 class ClaimValidator(ticket: List<List<Int>>) {
 
     private val topRowElements = ticket[TOP_ROW_INDEX].toSet()
+    private val middleRowElements = ticket[MIDDLE_ROW_INDEX].toSet()
     private val bottomRowElements = ticket[BOTTOM_ROW_INDEX].toSet()
 
     private val allElements = mutableSetOf<Int>()
@@ -31,6 +34,22 @@ class ClaimValidator(ticket: List<List<Int>>) {
                 count++
             }
             if (count == topRowElements.size) {
+                return index == (numbersAnnounced.size - ONE)
+            }
+        }
+
+        return false
+    }
+
+
+    private fun middleRowValidator(numbersAnnounced: List<Int>): Boolean {
+        var count = ZERO
+
+        numbersAnnounced.forEachIndexed { index, value ->
+            if (value in middleRowElements) {
+                count++
+            }
+            if (count == middleRowElements.size) {
                 return index == (numbersAnnounced.size - ONE)
             }
         }
@@ -71,6 +90,7 @@ class ClaimValidator(ticket: List<List<Int>>) {
     fun validate(numbersAnnounced: List<Int>, claim: String) =
         when (claim) {
             TOP_ROW -> topRowValidator(numbersAnnounced)
+            MIDDLE_ROW -> middleRowValidator(numbersAnnounced)
             BOTTOM_ROW -> bottomRowValidator(numbersAnnounced)
             FIRST_FIVE -> firstFiveValidator(numbersAnnounced)
             else -> throw Exception("Invalid claim made")
