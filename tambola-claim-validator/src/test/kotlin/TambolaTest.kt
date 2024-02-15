8ab95c091a7cac2e9c4e11de9d7211dc7e18be9c
SuryatejaChakkilala <chakkilalasuryatejab@sahaj.ai>
2024-02-15 11:14:32 +0530

    refactored unit tests

--- tambola-claim-validator/src/test/kotlin/TambolaTest.kt
+++ tambola-claim-validator/src/test/kotlin/TambolaTest.kt
@@ -1,3 +1,6 @@
+import Constants.ClaimConstants.BOTTOM_ROW
+import Constants.ClaimConstants.FIRST_FIVE
+import Constants.ClaimConstants.TOP_ROW
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
@@ -13,7 +16,7 @@ class TambolaTest {
         )
 
         val numbersAnnounced = listOf(90, 4, 46, 63, 89, 16, 76, 48)
-        val claimMade = "TOP_ROW"
+        val claimMade = TOP_ROW
 
         val result = ClaimValidator(ticket).validate(numbersAnnounced, claimMade)
         assertTrue(result)
@@ -28,7 +31,7 @@ class TambolaTest {
         )
 
         val numbersAnnounced = listOf(90, 4, 46, 63, 89, 16, 76, 48, 55, 1)
-        val claimMade = "TOP_ROW"
+        val claimMade = TOP_ROW
 
         val result = ClaimValidator(ticket).validate(numbersAnnounced, claimMade)
         assertFalse(result)
@@ -44,7 +47,7 @@ class TambolaTest {
         )
 
         val numbersAnnounced = listOf(90, 9, 44, 45, 25, 64, 22, 56, 88, 83)
-        val claimMade = "BOTTOM_ROW"
+        val claimMade = BOTTOM_ROW
 
         val result = ClaimValidator(ticket).validate(numbersAnnounced, claimMade)
         assertTrue(result)
@@ -59,7 +62,7 @@ class TambolaTest {
         )
 
         val numbersAnnounced = listOf(90, 9, 44, 45, 25, 64, 22, 56, 88, 83, 11, 12, 13)
-        val claimMade = "BOTTOM_ROW"
+        val claimMade = BOTTOM_ROW
 
         val result = ClaimValidator(ticket).validate(numbersAnnounced, claimMade)
         assertFalse(result)
@@ -74,7 +77,22 @@ class TambolaTest {
         )
 
         val numbersAnnounced = listOf(11, 12, 4, 23, 38, 23, 24, 25, 47, 48)
-        val claimMade = "FIRST_FIVE"
+        val claimMade = FIRST_FIVE
+
+        val result = ClaimValidator(ticket).validate(numbersAnnounced, claimMade)
+        assertFalse(result)
+    }
+
+    @Test
+    fun `player made claim of first five after missing to claim immediately after crossing the elements of first five should be rejected`() {
+        val ticket = listOf(
+            listOf(4, 16, 48, 63, 76),
+            listOf(7, 23, 38, 52, 80),
+            listOf(9, 25, 56, 64, 83)
+        )
+
+        val numbersAnnounced = listOf(4, 7, 9, 16, 22, 99, 25, 1, 2, 3)
+        val claimMade = FIRST_FIVE
 
         val result = ClaimValidator(ticket).validate(numbersAnnounced, claimMade)
         assertFalse(result)
