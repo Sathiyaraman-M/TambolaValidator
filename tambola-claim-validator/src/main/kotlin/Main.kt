7575c2ba2888b13ee2699164bd5c2795d6ca742e
Sathiyaraman-M <sathiyaraman2003@gmail.com>
2024-02-16 16:45:08 +0530

    Refactor MainKt to use a working TOP_ROW claim validation sample

--- tambola-claim-validator/src/main/kotlin/Main.kt
+++ tambola-claim-validator/src/main/kotlin/Main.kt
@@ -1,3 +1,10 @@
+import Constants.ClaimConstants.TOP_ROW
+
 fun main() {
-    ClaimValidator().validate(TambolaTicket(setOf(1, 2, 3, 4, 5), setOf(1, 2, 3, 4, 5), setOf(1, 2, 3, 4, 5)), numbersAnnounced = listOf(), "CLAAAIM")
+    val ticket = TambolaTicket(setOf(1, 2, 3, 4, 5), setOf(1, 2, 3, 4, 5), setOf(1, 2, 3, 4, 5))
+    val announcements = listOf(1, 2, 3, 4, 5)
+    val claim = TOP_ROW
+    val validator = ClaimValidator()
+    val result = validator.validate(ticket, announcements, claim)
+    println(if(result) "Accepted" else "Rejected")
 }
