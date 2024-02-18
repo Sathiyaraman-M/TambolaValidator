bc1837581be22ed6e24dcbe314fbc2c1afb6cc58
Sathiyaraman-M <sathiyaraman2003@gmail.com>
2024-02-18 11:34:31 +0530

    Update sample in MainKt to use the new implementations

--- tambola-claim-validator/src/main/kotlin/Main.kt
+++ tambola-claim-validator/src/main/kotlin/Main.kt
@@ -1,12 +1,15 @@
-import org.tambola.claimvalidator.Constants.ClaimConstants.TOP_ROW
-import org.tambola.claimvalidator.ClaimValidator
 import org.tambola.claimvalidator.ticket.TambolaTicket
+import org.tambola.claimvalidator.ticket.TambolaTicketRow
+import org.tambola.claimvalidator.validators.TopRowValidator
 
 fun main() {
-    val ticket = TambolaTicket(setOf(1, 2, 3, 4, 5), setOf(1, 2, 3, 4, 5), setOf(1, 2, 3, 4, 5))
+    val ticket = TambolaTicket(
+        TambolaTicketRow(1, 2, 3, 4, 5),
+        TambolaTicketRow(1, 2, 3, 4, 5),
+        TambolaTicketRow(1, 2, 3, 4, 5)
+    )
     val announcements = listOf(1, 2, 3, 4, 5)
-    val claim = TOP_ROW
-    val validator = ClaimValidator()
-    val result = validator.validate(ticket, announcements, claim)
+    val validator = TopRowValidator()
+    val result = validator.validate(ticket, announcements)
     println(if(result) "Accepted" else "Rejected")
 }
