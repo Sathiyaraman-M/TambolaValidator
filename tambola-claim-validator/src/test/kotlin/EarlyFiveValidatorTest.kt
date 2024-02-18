b6311dc54854663e53fcf1f7fb5678415acf1dcc
Sathiyaraman-M <sathiyaraman2003@gmail.com>
2024-02-18 10:00:26 +0530

    Complete EarlyFiveValidator Implementation and tests

--- tambola-claim-validator/src/test/kotlin/EarlyFiveValidatorTest.kt
+++ tambola-claim-validator/src/test/kotlin/EarlyFiveValidatorTest.kt
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class EarlyFiveValidatorTest {
@@ -10,12 +9,27 @@ class EarlyFiveValidatorTest {
         setOf(9, 25, 56, 64, 83)
     )
 
-
     @Test
     fun `player made claim of early five immediately after crossing the first five elements should be accepted`() {
         validator = EarlyFiveValidator()
-        val announcedNumbers = listOf(11, 12, 4, 23, 38, 23, 24, 25, 47, 48)
+        val announcedNumbers = listOf(11, 12, 4, 23, 38, 21, 24, 25, 47, 48)
         val result = validator.validate(ticket, announcedNumbers)
         assertEquals(true, result)
+    }
+
+    @Test
+    fun `player made claim of early five after missing to claim immediately after crossing the first five elements should be rejected`() {
+        validator = EarlyFiveValidator()
+        val announcedNumbers = listOf(11, 12, 4, 23, 38, 21, 24, 25, 47, 48, 2)
+        val result = validator.validate(ticket, announcedNumbers)
+        assertEquals(false, result)
+    }
+
+    @Test
+    fun `player made false claim of first five should be rejected`() {
+        validator = EarlyFiveValidator()
+        val announcedNumbers = listOf(1, 2, 4, 5, 7, 9)
+        val result = validator.validate(ticket, announcedNumbers)
+        assertEquals(false, result)
     }
 }
